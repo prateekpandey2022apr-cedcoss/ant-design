07df6d175e0114cffe08cdf71e3570ecffdfeb2f
prateekpandey2022apr-cedcoss <prateekpandey2022apr@cedcoss.com>
2022-11-22 18:15:44 +0530

    modification done

--- src/Listing.js
+++ src/Listing.js
@@ -1,16 +1,19 @@
-import { Space, Switch, Table } from "antd";
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
+import { Divider, Radio, Table } from "antd";
+
 const columns = [
   {
     title: "Name",
     dataIndex: "name",
-    key: "name",
+    render: (text) => <a>{text}</a>,
   },
   {
     title: "Username",
     dataIndex: "username",
-    key: "username",
-    width: "12%",
+  },
+  {
+    title: "Website",
+    dataIndex: "website",
   },
   {
     title: "Action",
@@ -20,51 +23,128 @@ const columns = [
   },
 ];
 
-// rowSelection objects indicates the need for row selection
-const rowSelection = {
-  onChange: (selectedRowKeys, selectedRows) => {
-    console.log(
-      `selectedRowKeys: ${selectedRowKeys}`,
-      "selectedRows: ",
-      selectedRows
-    );
+const data = [
+  {
+    key: 1,
+    name: "John Brown sr.",
+    age: 60,
+    address: "New York No. 1 Lake Park",
+    children: [
+      {
+        key: 11,
+        name: "John Brown",
+        age: 42,
+        address: "New York No. 2 Lake Park",
+      },
+      {
+        key: 12,
+        name: "John Brown jr.",
+        age: 30,
+        address: "New York No. 3 Lake Park",
+        children: [
+          {
+            key: 121,
+            name: "Jimmy Brown",
+            age: 16,
+            address: "New York No. 3 Lake Park",
+          },
+        ],
+      },
+      {
+        key: 13,
+        name: "Jim Green sr.",
+        age: 72,
+        address: "London No. 1 Lake Park",
+        children: [
+          {
+            key: 131,
+            name: "Jim Green",
+            age: 42,
+            address: "London No. 2 Lake Park",
+            children: [
+              {
+                key: 1311,
+                name: "Jim Green jr.",
+                age: 25,
+                address: "London No. 3 Lake Park",
+              },
+              {
+                key: 1312,
+                name: "Jimmy Green sr.",
+                age: 18,
+                address: "London No. 4 Lake Park",
+              },
+            ],
+          },
+        ],
+      },
+    ],
   },
-  onSelect: (record, selected, selectedRows) => {
-    console.log(record, selected, selectedRows);
+  {
+    key: 2,
+    name: "Joe Black",
+    age: 32,
+    address: "Sidney No. 1 Lake Park",
   },
-  onSelectAll: (selected, selectedRows, changeRows) => {
-    console.log(selected, selectedRows, changeRows);
-  },
-};
+];
+
 const Listing = () => {
+  const [selectedRowKeys, setSelectedRowKeys] = useState([]);
+  const [posts, setPosts] = useState();
+
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
       .then((response) => {
         console.log(response);
-        setPosts([...response.slice(0, 5)]);
+        // debugger;
+        // setPosts([...response.slice(0, 5)]);
+        setPosts(
+          [...response.slice(0, 5)].map((post) => {
+            return { key: post.id, ...post };
+          })
+        );
       });
   }, []);
 
-  const [posts, setPosts] = useState();
-  const [checkStrictly, setCheckStrictly] = useState(false);
+  // rowSelection object indicates the need for row selection
+  const rowSelection = {
+    onChange: (selectedRowKeys, selectedRows) => {
+      debugger;
+      console.log(
+        `selectedRowKeys: ${selectedRowKeys}`,
+        "selectedRows: ",
+        selectedRows
+      );
+    },
+    getCheckboxProps: (record) => ({
+      disabled: record.name === "Disabled User",
+      // Column configuration not to be checked
+      name: record.name,
+    }),
+  };
+
+  const [selectionType, setSelectionType] = useState("checkbox");
   return (
-    <>
-      <Space
-        align="center"
-        style={{
-          marginBottom: 16,
+    <div>
+      {/* <Radio.Group
+        onChange={({ target: { value } }) => {
+          setSelectionType(value);
         }}
+        value={selectionType}
       >
-        CheckStrictly:{" "}
-        <Switch checked={checkStrictly} onChange={setCheckStrictly} />
-      </Space>
+        <Radio value="checkbox">Checkbox</Radio>
+        <Radio value="radio">radio</Radio>
+      </Radio.Group> */}
+
+      <Divider />
+
       <Table
-        columns={columns}
         rowSelection={{
+          type: selectionType,
           ...rowSelection,
-          checkStrictly,
         }}
+        columns={columns}
         onRow={(record, rowIndex) => {
           return {
             onClick: (event) => {
@@ -80,9 +160,23 @@ const Listing = () => {
             onMouseLeave: (event) => {}, // mouse leave row
           };
         }}
+        expandable={{
+          expandedRowRender: (record) => (
+            <p
+              style={{
+                margin: 0,
+              }}
+            >
+              {Object.values(record.address)
+                .join(" ")
+                .replace("[object Object]", "")}
+            </p>
+          ),
+          rowExpandable: (record) => record.website !== "hildegard.org",
+        }}
         dataSource={posts}
       />
-    </>
+    </div>
   );
 };
 export default Listing;
